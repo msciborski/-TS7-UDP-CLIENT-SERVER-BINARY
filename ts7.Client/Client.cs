34cca0679dcedcd5b0e4316f3175fa10f4749160
msciborski <michal.sciborski@student.put.poznan.pl>
2017-11-17 17:52:08 +0100

    Add project files.

---
+++ ts7.Client/Client.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ts7.Client {
+    class Program{
+        private static Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        private static IPEndPoint ep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 11000);
+        private static EndPoint remoEndPoint;
+
+        static void Main(string[] args){
+            remoEndPoint = (EndPoint) ep;
+            s.Bind(remoEndPoint);
+            Thread thread = new Thread(DataIN);
+            thread.Start();
+            while (true){
+                try{
+                    string input = Console.ReadLine();
+                    byte[] bytes = Encoding.ASCII.GetBytes(input);
+                    s.SendTo(bytes, ep);
+                }
+                catch (Exception e){
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            //IPAddress broadcast = IPAddress.Parse("127.0.0.1");
+            //while (true){
+            //    var msg = Console.ReadLine();
+            //    byte[] bytes = Encoding.ASCII.GetBytes(msg);
+            //    IPEndPoint ep = new IPEndPoint(broadcast, 11000);
+            //    EndPoint remoteEndPoint = (EndPoint) ep;
+            //    s.SendTo(bytes, ep);
+            //    Console.WriteLine("Message sent!");
+            //    byte[] recvBytes = new byte[128];
+            //    try{
+            //        s.ReceiveFrom(recvBytes, SocketFlags.None, ref remoteEndPoint);
+            //        Console.WriteLine(Encoding.ASCII.GetString(recvBytes));
+            //    }
+            //    catch (Exception e){
+            //        Console.WriteLine("System disconnected.");
+            //    }
+
+            //}
+        }
+
+        private static void DataIN(){
+            byte[] recvBytes = new byte[128];
+
+            while (true){
+                try{
+                    s.ReceiveFrom(recvBytes, SocketFlags.None, ref remoEndPoint);
+                    Console.WriteLine("Message from server: {0}", Encoding.ASCII.GetString(recvBytes));
+                }
+                catch (SocketException e){
+                    Console.WriteLine("Server closed!");
+                }
+            }
+        }
+    }
+}
---
+++ ts7.Server/Server.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ts7.Server {
+    class Server{
+        private const int listenPort = 11000;
+        private static UdpClient listener = new UdpClient(listenPort);
+        private static Dictionary<IPEndPoint, string> _players = new Dictionary<IPEndPoint, string>();
+        private static List<ClientData> _playersData;
+        static IPEndPoint groupEP = new IPEndPoint(IPAddress.Any, listenPort);
+
+        private static void ListenForPlayers(){
+            //while (_players.Count < 2){
+                Console.WriteLine("Waiting for players.");
+                byte[] bytes = listener.Receive(ref groupEP);
+                IPEndPoint playerEndPoint = new IPEndPoint(groupEP.Address,groupEP.Port);
+                string playerName = Encoding.ASCII.GetString(bytes);
+                if (_players.ContainsKey(playerEndPoint)){
+                    byte[] msg = Encoding.ASCII.GetBytes("Player exist.");
+                    listener.Send(msg, msg.Length, playerEndPoint);
+                }
+                else{
+                    byte[] msg = Encoding.ASCII.GetBytes(String.Format("Player {0} added;{1}", playerName,
+                        playerEndPoint.ToString()));
+                    listener.Send(msg, msg.Length, playerEndPoint);
+                    _playersData.Add(new ClientData(playerEndPoint));
+                    _players.Add(playerEndPoint,playerName);
+                }
+
+            //}
+        }
+
+        public static void DataIn(object ep){
+            IPEndPoint endPoint = (IPEndPoint) ep;
+            int readBytes;
+            while (true){
+                try{
+                    byte[] buffer = listener.Receive(ref endPoint);
+                    Console.WriteLine(Encoding.ASCII.GetString(buffer));
+                    ProcessData();
+                }
+                catch (Exception e){
+                    Console.WriteLine("Client disconnected.");
+                }
+            }
+        }
+
+        public static void ProcessData(){
+            
+        }
+
+        private static void sendTimeInformationToEveryone(){
+            while (true){
+                var time = DateTime.Now.ToShortTimeString();
+                foreach (var player in _players){
+                    byte[] data = Encoding.ASCII.GetBytes(time);
+                    listener.Send(data, data.Length, player.Key);
+                }
+                Thread.Sleep(2000);
+            }
+        }
+
+        private static void StartGame(){
+            Thread timeThread = new Thread(sendTimeInformationToEveryone);
+            timeThread.Start();
+        }
+        static void Main(string[] args) {
+            ListenForPlayers();
+            StartGame();
+        }
+
+    }
+
+    class ClientData{
+        private Thread dataInClient;
+        private IPEndPoint clientEndPoint;
+
+        public ClientData(IPEndPoint ep){
+            clientEndPoint = ep;
+            dataInClient = new Thread(Server.DataIn);
+            dataInClient.Start(clientEndPoint);
+        }
+    }
+}
